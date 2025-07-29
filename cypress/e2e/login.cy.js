af1478e966f58ec7ea31b1b198a5e68c4828244e
Vasilica Cernovschi <vasilica.cernovschi@assist.ro>
2025-07-29 23:52:52 +0300

    new line added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,6 +8,7 @@ describe('Login Page Tests', () => {
     cy.get(".authorization-link a").contains("Sign In").click();
     cy.get("#email").clear().type("vasilica.cernovschi1234@gmail.com");
     cy.get("#pass").clear().type("tR23.xn6jwr@.nC");
+    cy.get("#sendEdit").click();
     cy.get("#send2").click();
     cy.get(".logged-in").should("be.visible").and("contain.text", "Welcome");
   })
